7bd2360a0e5d19742954e701338acaa8355863cd
lsuazo <luisrsuazo@gmail.com>
2024-01-10 17:03:28 -0600

    [nvim lua] working on autocomplete and snippets

--- nvim_lua_setup/lua/plugins/lsp-config.lua
+++ nvim_lua_setup/lua/plugins/lsp-config.lua
@@ -15,11 +15,20 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.clangd.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
--- nvim_lua_setup/lua/plugins/treesitter.lua
+++ nvim_lua_setup/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-        ensure_installed = { "c", "lua", "vim", "python" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			auto_install = true,
+			ensure_installed = { "c", "lua", "vim", "python" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
